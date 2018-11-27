d402e7690d79a77a8e58f58384bba49d401fed38
jadhavganesh <jadhavganesh0301@gmail.com>
2018-11-27 12:55:23 +0530

    Create Form

---
+++ poll/forms.py
@@ -0,0 +1,23 @@
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit
+from django import forms
+from .models import Snippet
+
+class ContactForm(forms.Form):
+	name = forms.CharField(label='First Name')
+	last_name = forms.CharField(label='Last Name')
+	email = forms.EmailField(label='Email')    
+	mobile = forms.CharField(label='Mobile')
+	
+	
+	
+	
+		
+		
+	
+	
+class SnippetForm(forms.ModelForm):
+
+	class Meta:
+		model = Snippet
+		fields = ('name','last_name','email','mobile')
